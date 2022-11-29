e4cf0cc39e43bf0bc0e8efe217d846a576cbde96
Nisar Ahmed <juniorchannel54@gmail.com>
2022-11-29 00:10:30 +0000

    final

--- test1 quick version real.py
+++ test1 quick version real.py
@@ -3,33 +3,30 @@ import random
 
 point_1 = 0
 point_2 = 0
-rank_names = ["high card", "pair", "two pair", "three of a kind", "straight", "flush", "full house",
-"four of a kind", "straight flush"]
 
-"""
+
+
 deck = ["AC","2C","3C","4C","5C","6C","7C","9C","8C","9C","10C","JC","QC","KC",
         "AD","2D","3D","4D","5D","6D","7D","9D","8D","9D","10D","JD","QD","KD",
         "AH","2H","3H","4H","5H","6H","7H","9H","8H","9H","10H","JH","QH","KH",
         "AS","2S","3S","4S","5S","6S","7S","9S","8S","9S","10S","JS","QS","KS" ]
 
-"""
 
+#Shuffles the deck
 def shuffle():
     random.shuffle(deck)
-    print(deck)
-
+        
 hand1 = []
 hand2 = []
 
-
+#Assigns 5 cards to each player from the shuffled deck 
 def give_hands():
-    count = 0
-    while count <= 4 :
+    for i in range(5):
         hand1.append(deck[0])
         deck.remove(deck[0])
         hand2.append(deck[0])
         deck.remove(deck[0])
-        count += 1
+    
 
 
 def suit(card):
@@ -95,74 +92,49 @@ def hand_rank(cards):
 def compare():
     if x <= y:
         point_2 += 1
-        print("Player 2 wins")
-        print(point_2_)
     if x>= y:
         point_1 += 1
-        print("Player 1 wins")
-        print (point_1)
-    else:
-        print("Tie")
 
 def rank():
     global point_1
     global point_2
     x = str(hand_rank(hand1))
-    print(x)
     y = str(hand_rank(hand2))
-    print(y)
     if x < y:
         point_2 += 1
-        print("Player 2 wins")
     if x> y:
         point_1 += 1
-        print("Player 1 wins")
-    if x == y:
-        print("Tie")
-    print("Player 1 has ", point_1, "point/s")
-    print("Player 2 has ", point_2, " point/s")
 
 
-def make_deck():
-  deck = []
-  for suit in ("D", "C", "H", "S"):
-    for value in range(2, 15):
-      if value < 11:
-        value_string = str(value)
-      else:
-        value_string = ("J", "Q", "K", "A")[value - 11]
-      deck.append(value_string + suit)
-  return deck
-
-def show_compare_hands(hand1, hand2):
-  sgn = compare(hand1, hand2)
-  result = ("loses to", "ties", "beats")[sgn + 1]
-  print(f'{hand1} {result} {hand2}')
-  r1 = rank(hand1)
-  r2 = rank(hand2)
-  print(f'{rank_names[r1]} {result} {rank_names[r2]}')
-
-def deal(deck, n):
-  hand = deck[0:n]
-  del deck[0:n]
-  return hand
-
-def shuffled_deck():
-  deck = make_deck()
-  random.shuffle(deck)
-  return deck
-
-def rank_distribution(n=100000):
-  dist = {i: 0 for i in range(9)}
-  for i in range(n):
-    deck = shuffled_deck()
-    hand = deal(deck, 5)
-    dist[hand_rank(hand)] += 1
-
-  for r in range(9):
-    print(f'{rank_names[r]}: {dist[r]} ({100 * dist[r] / n}%)')
-
-rank_distribution()
+"""
+shuffle()
+give_hands()
+hand_rank(hand1)
+hand_rank(hand2)
+rank()
+point_1 += 1
+point_2 += 1
+print(point_2)
+print(point_1)
+"""
 
 
+
+for i in range(100000):
+    shuffle()
+
+    give_hands()
+    hand_rank(hand1)
+    hand_rank(hand2)
+    rank()
+    hand1 = []
+    hand2 = []
+    deck = ["AC","2C","3C","4C","5C","6C","7C","9C","8C","9C","10C","JC","QC","KC",
+        "AD","2D","3D","4D","5D","6D","7D","9D","8D","9D","10D","JD","QD","KD",
+        "AH","2H","3H","4H","5H","6H","7H","9H","8H","9H","10H","JH","QH","KH",
+        "AS","2S","3S","4S","5S","6S","7S","9S","8S","9S","10S","JS","QS","KS" ]
+
+
+print("Player 1 has ", point_1, "point/s")
+print("Player 2 has ", point_2, " point/s")
 
